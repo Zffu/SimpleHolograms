c1af0c6c57cdddfc8ca3881a8ef66f9ae6997be4
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-08-15 22:21:06 +0200

    Added basic api system!

---
+++ src/main/java/gg/zffu/simpleholograms/api/SimpleHologramsAPI.java
@@ -0,0 +1,13 @@
+package gg.zffu.simpleholograms.api;
+
+import org.bukkit.plugin.Plugin;
+
+public class SimpleHologramsAPI {
+
+    private Plugin plugin;
+
+    public SimpleHologramsAPI(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+}
--- src/main/java/gg/zffu/simpleholograms/plugin/SimpleHologramPlugin.java
+++ src/main/java/gg/zffu/simpleholograms/plugin/SimpleHologramPlugin.java
@@ -1,13 +1,17 @@
 package gg.zffu.simpleholograms.plugin;
 
+import gg.zffu.simpleholograms.api.SimpleHologramsAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleHologramPlugin extends JavaPlugin {
 
+    private SimpleHologramsAPI api;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        // Init the plugin API
+        this.api = new SimpleHologramsAPI(this);
     }
 
     @Override
