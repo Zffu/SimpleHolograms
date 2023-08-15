efffd4e9bc9ece958108257fddc47f608326dbb0
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-08-15 22:12:44 +0200

    Added Basic Code!

---
+++ src/main/java/gg/zffu/simpleholograms/plugin/SimpleHologramPlugin.java
@@ -0,0 +1,17 @@
+package gg.zffu.simpleholograms.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleHologramPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
