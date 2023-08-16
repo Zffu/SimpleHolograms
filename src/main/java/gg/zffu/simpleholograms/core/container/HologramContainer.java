862af6d5b8532c0f27a1e415c92d4c75563a9f93
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-08-16 10:02:06 +0200

    Added Hologram Container code

--- src/main/java/gg/zffu/simpleholograms/core/container/HologramContainer.java
+++ src/main/java/gg/zffu/simpleholograms/core/container/HologramContainer.java
@@ -1,4 +1,18 @@
 package gg.zffu.simpleholograms.core.container;
 
+import gg.zffu.simpleholograms.core.holograms.Hologram;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class HologramContainer {
+
+    private List<Hologram> holograms = new ArrayList<>();
+
+    public HologramContainer() {
+
+    }
+
 }
---
+++ src/main/java/gg/zffu/simpleholograms/core/holograms/Hologram.java
@@ -0,0 +1,4 @@
+package gg.zffu.simpleholograms.core.holograms;
+
+public interface Hologram {
+}
