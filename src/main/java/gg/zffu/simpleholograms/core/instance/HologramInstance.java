e2975bd3463cbcab0be1b9eb6e361978acc6d843
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-08-16 10:08:45 +0200

    Added some basic code in HologramInstance

--- src/main/java/gg/zffu/simpleholograms/core/instance/HologramInstance.java
+++ src/main/java/gg/zffu/simpleholograms/core/instance/HologramInstance.java
@@ -1,7 +1,21 @@
 package gg.zffu.simpleholograms.core.instance;
 
+import gg.zffu.simpleholograms.core.holograms.Hologram;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class HologramInstance {
 
+    private Hologram hologram;
+    private List<Object> entities;
 
+    private List<Player> viewing = new ArrayList<>();
+
+    public HologramInstance(Hologram hologram) {
+        this.hologram = hologram;
+    }
 
 }
