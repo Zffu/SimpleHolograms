d8928008060bd5442e80a5d3a9d226024796f596
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-08-16 10:04:20 +0200

    Added HologramLines for storing lines

--- src/main/java/gg/zffu/simpleholograms/core/holograms/Hologram.java
+++ src/main/java/gg/zffu/simpleholograms/core/holograms/Hologram.java
@@ -1,4 +1,9 @@
 package gg.zffu.simpleholograms.core.holograms;
 
+import com.avaje.ebean.validation.NotNull;
+
 public interface Hologram {
+
+
+
 }
---
+++ src/main/java/gg/zffu/simpleholograms/core/lines/HologramLines.java
@@ -0,0 +1,25 @@
+package gg.zffu.simpleholograms.core.lines;
+
+import gg.zffu.simpleholograms.core.holograms.Hologram;
+
+import java.util.ArrayList;
+
+public class HologramLines {
+
+    private ArrayList<String> lines = new ArrayList<>();
+
+    // For Updating
+    private Hologram hologram;
+
+    public HologramLines(Hologram hologram) {
+        this.hologram = hologram;
+    }
+
+    public void addLine(String line) {
+        this.lines.add(line);
+    }
+
+
+
+
+}
